e5d97503e77325bee7f57a1069ef8629cc6ac0c5
nilxdev <thedev@nilxdev.com>
2024-07-03 16:42:35 +0100

    chore: change package name

--- _examples/prompt.go
+++ _examples/prompt.go
@@ -3,7 +3,7 @@ package main
 import (
 	"log"
 
-	"github.com/jesseduffield/gocui"
+	"github.com/nilxdev/gocui"
 )
 
 func main() {
--- go.mod
+++ go.mod
@@ -1,4 +1,4 @@
-module github.com/jesseduffield/gocui
+module github.com/nilxdev/gocui
 
 go 1.12
 
